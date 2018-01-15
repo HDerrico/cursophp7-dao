1a0ffda19a6a91f7f7e1f2a51241b7a62be8a909
Henrique <henriqueferrazderrico@sjrp.unesp.br>
2018-01-15 10:52:31 -0200

    Inserir e alterar

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,12 +1,13 @@
 <?php
 
 class Usuario{
-
+	//Atributos
 	private $idusuario;
 	private $deslogin;
 	private $dessenha;
 	private $dtcadastro;
 
+	//INICIO: GETTERS AND SETTERS
 	public function getId(){
 		return $this->idusuario;
 	}
@@ -37,6 +38,11 @@ class Usuario{
 	public function setCadastro($value){
 		$this->dtcadastro = $value;
 	}
+	//TERMINO: GETTERS AND SETTERS
+
+
+	//INICIO: MÉTODOS DE LISTAS
+
 
 	//Carrega informaçoes de um usuario pelo ID.
 	public function loadById($id){
@@ -44,53 +50,85 @@ class Usuario{
 
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));	//Comando select na tabela tb_usuarios buscando por ID, retorna um array.
 
-		if(count($results) > 0){	//Se a primeira linha do array existir, ou seja, se tiver encontrado o usuario.
-			$row = $results[0];
-
-			//Coloca as informaçoes carregadas do banco nos atributos do objeto.
-			$this->setId($row['idusuario']);
-			$this->setLogin($row['deslogin']);
-			$this->setSenha($row['dessenha']);
-			$this->setCadastro(new DateTime($row['dtcadastro']));
+		if(count($results) > 0){	//Se existir informaçoes em results 1 usuario foi encontrado.
+			$this->setDados($results[0]);
 		}
 	}
 
 
 	//Nao é necessario instanciar um objeto para chamar esse método, por isso ele é estático.
+	//Carrega uma lista de usuarios do banco, ordenando pelo login.
 	public static function getList(){
 		$sql = new Sql();
 		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
 	}
 
-
+	//Nao é necessario instanciar um objeto para chamar esse método, por isso ele é estático.
+	//Carrega uma lista de usuario do banco, buscando pelo login ou parte do login.
 	public static function search($login){
 		$sql = new Sql();
 		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
-			':SEARCH'=>"%".$login."%"
+			':SEARCH'=>"%".$login."%"	//Os '%' servem para colocar aspas simples, permitindo a busca por partes do login.
 		));
 	}
 
-
+	//Carrega um usuario do banco pelo login e senha.
 	public function login($login, $password){
 		$sql = new Sql();	//Objeto Sql
 
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
 			":LOGIN"=>$login,
 			":PASSWORD"=>$password
-		));	//Comando select na tabela tb_usuarios buscando por ID, retorna um array.
+		));	//Comando select na tabela tb_usuarios buscando por login e senha, retorna um array.
 
-		if(count($results) > 0){	//Se a primeira linha do array existir, ou seja, se tiver encontrado o usuario.
-			$row = $results[0];
-
-			//Coloca as informaçoes carregadas do banco nos atributos do objeto.
-			$this->setId($row['idusuario']);
-			$this->setLogin($row['deslogin']);
-			$this->setSenha($row['dessenha']);
-			$this->setCadastro(new DateTime($row['dtcadastro']));
+		if(count($results) > 0){	//Se existirem informaçoes no array, ou seja, se tiver encontrado o usuario.
+			$this->setDados($results[0]); //Coloca as informaçoes carregadas do banco nos atributos do objeto.
 		}else{
-			throw new Exception("LOGIN E/OU SENHA INVALIDOS!", 1);
-			
+			throw new Exception("LOGIN E/OU SENHA INVALIDOS!", 1);	//Erro, nao encontrou nenhum usuario com essa login/senha
 		}
+	}
+	//TERMINO: MÉTODOS DE LISTAS
+
+
+	public function setDados($dados){
+		$this->setId($dados['idusuario']);
+		$this->setLogin($dados['deslogin']);
+		$this->setSenha($dados['dessenha']);
+		$this->setCadastro(new DateTime($dados['dtcadastro']));
+	}
+
+
+	public function insert(){
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getLogin(),
+			':PASSWORD'=>$this->getSenha()
+		));
+
+		if(count($results) > 0){
+			$this->setDados($results[0]);
+		}
+	}
+
+
+	public function update($login, $password){
+
+		$this->setLogin($login);
+		$this->setSenha($password);
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getLogin(),
+			':PASSWORD'=>$this->getSenha(),
+			':ID'=>$this->getId()
+			));
+	}
+
+
+	public function __construct($login = "", $password = ""){	//As aspas permitem instanciar objetos sem passar login e senha
+		$this->setLogin($login);
+		$this->setSenha($password);
 	}
 
 
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once("config.php");
 
 /*
-Carrega APENAS um usuario, pelo ID.
+//Carrega APENAS um usuario, pelo ID.
 $usuario = new Usuario();
 $usuario->loadById(12);
 echo $usuario;
@@ -11,29 +11,39 @@ echo $usuario;
 
 
 /*
-Carrega todos os usuarios.
+//Carrega todos os usuarios.
 $lista = Usuario::getList();
 echo json_encode($lista);
 */
 
 
 /*
-Carrega uma lista de usuarios buscando pelo login ou parte dele
+//Carrega uma lista de usuarios buscando pelo login ou parte dele
 $search = Usuario::search("Bi");
 echo json_encode($search);
 */
 
 
 /*
-Carrega um usuario pelo Login e Senha.
+//Carrega um usuario pelo Login e Senha.
 $usuario = new Usuario();
 $usuario->login("Bipe", "Orelha");
 echo ($usuario);
 */
 
+
 /*
-
-
-
+//Insere um usuario no banco
+$aluno = new Usuario("Bruno", "Zacarin");
+$aluno->insert();
+echo $aluno;
 */
+
+
+/*
+*/
+$usuario = new Usuario();
+$usuario->loadById(14);
+$usuario->update("Salsicha", "123hau2");
+echo $usuario;
 ?>
